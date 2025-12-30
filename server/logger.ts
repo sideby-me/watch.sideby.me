a7de661a4269ea589f70b0bf330b8a986f340378
Karthikeyan A <whonoah.exe@gmail.com>
2025-12-31 01:08:36 +0530

    feat: rem logtail

--- server/logger.ts
+++ server/logger.ts
@@ -1,13 +1,4 @@
-import { Logtail } from '@logtail/node';
-
-const token = process.env.LOGTAIL_SOURCE_TOKEN;
-const logtail = token ? new Logtail(token) : null;
-
 export function logVideoEvent(record: object) {
   const payload = { ...record, service: 'watch', ts: Date.now() };
-  if (logtail) {
-    logtail.info('video-event', payload);
-  } else {
-    console.log('[video-event]', JSON.stringify(payload));
-  }
+  console.log('[video-event]', JSON.stringify(payload));
 }
